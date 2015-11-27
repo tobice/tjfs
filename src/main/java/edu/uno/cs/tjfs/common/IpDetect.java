4db6f05b037d5b6cfcb455d4bb5b5eb7e106af27
Tobiáš Potoček <tobiaspotocek@gmail.com>
2015-11-27 15:47:14 -0600

    Updated IP detect so that it works with increasing tolerance when looking for suitable IP

--- src/main/java/edu/uno/cs/tjfs/common/IpDetect.java
+++ src/main/java/edu/uno/cs/tjfs/common/IpDetect.java
@@ -12,27 +12,31 @@ public class IpDetect {
      * As one computer can have multiple interfaces and each interface can have assigned multiple
      * IP addresses, it is difficult to get that "one" IP address we need. To get rid of the
      * ambiguity, the IP address that exists in the sam subnet as Zookeeper's IP address is
-     * returned. If Zookeeper and this machine are not in the same subnet then this method fails,
-     * but this requirement is reasonable as it is easy to meet.
+     * returned. If Zookeeper and this machine are not in the same subnet, we reduce the prefix
+     * length (mask) and try again. That way we return the IP that is "closest" to Zookeeper's
+     * address.
      *
      * Currently only Ipv4 is supported and tested.
      *
      * @param zookeeper IP address of Zookeeper instance that this computer is connected to.
      * @return local IP address that belongs to the same subnet as Zookeeper's IP address.
      * @throws TjfsException if the given Zookeeper's address is malformed, or if we fail to obtain
-     *  the list of current network interfaces and their IP addresses, or if there is no IP address
-     *  in the same subnet as Zookeeper's IP.
+     *  the list of current network interfaces and their IP addresses, or if there is not suitable
+     *  IP address found.
      */
     public static String getLocalIp(String zookeeper) throws TjfsException {
         try {
             byte[] zookeeperIp = InetAddress.getByName(zookeeper).getAddress();
-            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements(); ) {
-                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+            for (int prefixTolerance = 0; prefixTolerance <= 24; prefixTolerance += 8) {
+                for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements(); ) {
+                    NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
 
-                for (InterfaceAddress address : iface.getInterfaceAddresses()) {
-                    byte[] ip = address.getAddress().getAddress();
-                    if (sameSubnet(ip, zookeeperIp, address.getNetworkPrefixLength())) {
-                        return address.getAddress().getHostAddress();
+                    for (InterfaceAddress address : iface.getInterfaceAddresses()) {
+                        byte[] ip = address.getAddress().getAddress();
+                        int prefix = Math.min(8, address.getNetworkPrefixLength() - prefixTolerance);
+                        if (sameSubnet(ip, zookeeperIp, prefix)) {
+                            return address.getAddress().getHostAddress();
+                        }
                     }
                 }
             }
