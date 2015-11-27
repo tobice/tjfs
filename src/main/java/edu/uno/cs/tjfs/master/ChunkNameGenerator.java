882ec38fad5d6c99380875e2016dcc32c7948b04
Tobiáš Potoček <tobiaspotocek@gmail.com>
2015-11-27 16:19:03 -0600

    Fixed chunk name generator (it's producing nice hex strings)

--- src/main/java/edu/uno/cs/tjfs/master/ChunkNameGenerator.java
+++ src/main/java/edu/uno/cs/tjfs/master/ChunkNameGenerator.java
@@ -1,27 +1,46 @@
 package edu.uno.cs.tjfs.master;
 
 import edu.uno.cs.tjfs.common.BaseLogger;
-import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
-import sun.nio.ch.IOUtil;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class ChunkNameGenerator {
     final static Logger logger = BaseLogger.getLogger(ChunkNameGenerator.class);
-    // TODO: test that you get n different strings
+
     public static Set<String> generate(int number) {
         Set<String> names = new HashSet<>();
         while (names.size() != number) {
             try {
-                MessageDigest md = MessageDigest.getInstance("MD5");
                 String name = Thread.currentThread().getName() + System.currentTimeMillis() + UUID.randomUUID();
-                names.add(IOUtils.toString(md.digest(name.getBytes()), "UTF-8"));
+                names.add(md5(name));
             } catch (Exception e) {
                 logger.error("ChunkNameGenerator.generate - error while generating chunks");
             }
         }
         return names;
+    }
+
+    protected static String md5(String value) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(value.getBytes());
+            byte byteData[] = md.digest();
+
+            // Convert it to hex
+            StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < byteData.length; i++) {
+                String hex = Integer.toHexString(0xff & byteData[i]);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // As fallback use base64
+            return Base64.getEncoder().encodeToString(value.getBytes());
+        }
     }
 }
