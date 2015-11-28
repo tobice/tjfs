125959fe12f1d95e5a83ae143e0fba569b8c2ebf
Tobiáš Potoček <tobiaspotocek@gmail.com>
2015-11-27 18:19:50 -0600

    Updated config

--- src/main/java/edu/uno/cs/tjfs/Config.java
+++ src/main/java/edu/uno/cs/tjfs/Config.java
@@ -11,9 +11,9 @@ import java.util.Enumeration;
 
 public class Config {
     protected int chunkSize = 16 * 1024 * 1024;
-    protected int executorPoolSize = 3;
-    protected int executorQueueSize = 3;
-    protected int pipeBufferSize = 3 * chunkSize;
+    protected int executorPoolSize = 6;
+    protected int executorQueueSize = 10;
+    protected int pipeBufferSize = 5 * chunkSize;
     protected int zookeeperSessionTimeout = 1000;
     protected int masterReplicationIntervalTime = 10000;
 
